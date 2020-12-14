0440527e590ea7f436c4a5c5327ed6b5f3e12893
markoJ604 <markojovanovic604@gmail.com>
2020-12-14 21:01:38 +0100

    Section6 exercises

---
+++ src/Section6/Circle.java
@@ -0,0 +1,20 @@
+package Section6;
+
+public class Circle {
+
+    private double radius;
+    private static final double PI_CONSTANT = Math.PI;
+
+    public Circle(double radius) {
+        if(radius<0) this.radius = 0;
+        else this.radius = radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public double getArea(){
+        return radius*radius*PI_CONSTANT;
+    }
+}
---
+++ src/Section6/ComplexNumber.java
@@ -0,0 +1,41 @@
+package Section6;
+
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        this.real += complexNumber.real;
+        this.imaginary += complexNumber.imaginary;
+    }
+
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber){
+        this.real -= complexNumber.real;
+        this.imaginary -= complexNumber.imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+}
